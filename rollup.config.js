b3fe5892ec71c8f00626dbc8c482effc6447eaa0
Jason Miller <jasonmiller@synacor.com>
2017-06-06 22:29:09 -0400

    Inline dlv since it only adds 20 bytes and saves removes the only dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import buble from 'rollup-plugin-buble';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-	external: [ 'preact', 'dlv'],
+	external: ['preact'],
 	globals: {
 		preact: 'preact',
 		dlv: 'dlv'
 	},
 	plugins: [
+		nodeResolve({
+			jsnext: true
+		}),
 		buble({
 			jsx: 'h',
 			objectAssign: 'assign'
